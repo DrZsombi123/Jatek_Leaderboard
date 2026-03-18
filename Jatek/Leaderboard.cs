5f8848b1fd684104c0c8a88236b306567df7b471
DrZsombi123 <118626431+DrZsombi123@users.noreply.github.com>
2026-03-18 11:32:12 +0100

    Add project files.

---
+++ Jatek/Leaderboard.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jatek
+{
+    internal class Leaderboard
+    {
+        string name;
+        int score;
+        DateTime date;
+        List<Eredmeny> eredmenyek = new List<Eredmeny>();
+
+        public string Name { get => name; set => name = value; }
+        public int Score { get => score; set => score = value; }
+        public DateTime Date { get => date; set => date = value; }
+
+        public Leaderboard(string name, int score, DateTime date)
+        {
+            this.Name = name;
+            this.Score = score;
+            this.Date = date;
+        }
+
+        public void Rogzit(string nev, int pont) {
+            Eredmeny ujEredmeny = new Eredmeny(nev, pont);
+            eredmenyek.Add(ujEredmeny);
+        }
+        public List<string> GetBoard() { return eredmenyek; }
+
+        public void Reset() {
+            eredmenyek.Clear();
+        }
+
+
+    }
+}
