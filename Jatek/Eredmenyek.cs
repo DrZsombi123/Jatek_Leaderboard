923294c95f627b74f1099e49989877f2cfbeec88
mezeimarcell69420 <mezeimarcell2fa@gmail.com>
2026-03-18 11:34:54 +0100

    commit

---
+++ Jatek/Eredmenyek.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jatek
+{
+    public class Eredmeny
+    {
+        private string nev;
+        private int pont;
+        private DateTime idopont;
+
+        public DateTime Idopont => idopont;
+        public int Pontszam => pont;
+        public string Jatekos => nev;
+
+        public Eredmeny(string nev, int pont)
+        {
+            this.nev = nev;
+            this.pont = pont;
+            this.idopont = DateTime.Now;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Név: {nev} ;Pont: {pont}; Idő: {idopont}";
+        }
+    }
+}
