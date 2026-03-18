49898f7c439e0445ab7eee8db2d55fc0a53ba71d
DrZsombi123 <118626431+DrZsombi123@users.noreply.github.com>
2026-03-18 11:38:18 +0100

    dgdfg

--- Jatek/Program.cs
+++ Jatek/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            
+            Leaderboard leaderboard = new Leaderboard("Player1", 100, DateTime.Now);
+            leaderboard.Rogzit("Player1", 100);
+            leaderboard.Rogzit("Player2", 200);
+
+            List<Eredmeny> eredmenyek = leaderboard.GetBoard();
+            foreach (Eredmeny eredmeny in eredmenyek)
+            {
+                Console.WriteLine($"Név: {eredmeny.Nev}, Pont: {eredmeny.Pont}");
+            }
+
+            leaderboard.Reset();
         }
     }
 }
